7c04c459b519fa59676bd15332fa1b0f011f2b32
Ry0_Ka <k104037r@mail.kyutech.jp>
2015-11-24 22:27:09 +0900

    kadai1Bのwindows版作成

--- kadai1B/kadai.h
+++
@@ -1,6 +0,0 @@
-#ifndef _KADAI_DEF_
-#define _KADAI_DEF_
-#include "kadai1A.h"
-#include "kadai1B.h"
-#include "Struct.h"
-#endif
---
+++ kadai1B/kadai1B/Struct.h
@@ -0,0 +1,27 @@
+#ifndef _STRUCT_DEF_
+#define _STRUCT_DEF_
+
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <stdlib.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <time.h>
+
+typedef struct {
+	double x;
+	double y;
+	double z;
+} POINT;
+
+typedef struct {
+	POINT x_axis;
+	POINT y_axis;
+	POINT z_axis;
+	POINT origin;
+} AXIS;
+
+#define VEC_SIZE 4 // ベクトルのサイズ
+// #define DEBUG // デバッグ用のprintfを出力するか
+
+#endif
