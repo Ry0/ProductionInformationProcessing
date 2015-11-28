3b056a16131f8b8aa6628c2797b190400b8248ab
Ry0 <k104037r@mail.kyutech.jp>
2015-11-29 01:20:29 +0900

    課題2Aのレポート追加

--- kadai2A/main.cpp
+++ kadai2A/main.cpp
@@ -3,9 +3,9 @@
 int main(){
 
   // 課題1
-  // double tetra_pos[TETRA_V_NUM][VEC_SIZE];
-  // double triprism_pos[TRIPRISM_V_NUM][VEC_SIZE];
-  // stlb_k01(tetra_pos, triprism_pos);
+  double tetra_pos[TETRA_V_NUM][VEC_SIZE];
+  double triprism_pos[TRIPRISM_V_NUM][VEC_SIZE];
+  stlb_k01(tetra_pos, triprism_pos);
 
   // 課題2
   TransformPositionAll((char*)"Tetra-Triprism");
