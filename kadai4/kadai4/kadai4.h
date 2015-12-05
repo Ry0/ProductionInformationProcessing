65eef7d88996299dc823d74307662f6833767956
Ry0_Ka <k104037r@mail.kyutech.jp>
2015-12-06 02:52:03 +0900

    課題4が一応できたよ

--- kadai4/kadai4/kadai4.h
+++ kadai4/kadai4/kadai4.h
@@ -5,18 +5,18 @@
 #include <stdlib.h>
 #include "array_graph.h"
 
-/*
-g(O) グラフデータをまとめた配列．
-vs(I) グラフ頂点用配列．
-es(I) グラフ辺用配列．
-cs(I) グラフ辺リスト用リストセル配列． */
+// #define debug //デバッグ用のprintf
 
-int calc_earliest_node_times(int *g[GRAPH_ITEM_NUM], int vs[][VERTEX_PROPERTY], int es[][EDGE_PROPERTY], int cs[][CELL_PROPERTY], int task_num);
+// 任意の地点での最早結合点時刻を求める
+int calc_tEj(int *g[GRAPH_ITEM_NUM], int vs[][VERTEX_PROPERTY], int es[][EDGE_PROPERTY], int cs[][CELL_PROPERTY], int vid);
+// すべての地点での最早結合点時刻を画面出力する
+void calc_earliest_node_times(int *g[GRAPH_ITEM_NUM], int vs[][VERTEX_PROPERTY], int es[][EDGE_PROPERTY], int cs[][CELL_PROPERTY], int task_num);
 
-int calc_tj(int *g[GRAPH_ITEM_NUM], int vs[][VERTEX_PROPERTY], int es[][EDGE_PROPERTY], int cs[][CELL_PROPERTY], int eid);
+// 任意の地点での最遅結合点時刻を求める
+int calc_tLi(int *g[GRAPH_ITEM_NUM], int vs[][VERTEX_PROPERTY], int es[][EDGE_PROPERTY], int vid);
+// すべての地点での最遅結合点時刻を画面出力する
+void calc_latest_node_times(int *g[GRAPH_ITEM_NUM], int vs[][VERTEX_PROPERTY], int es[][EDGE_PROPERTY], int cs[][CELL_PROPERTY], int task_num);
 
-int calc_earliest_node_times(int *g[GRAPH_ITEM_NUM], int vs[][VERTEX_PROPERTY], int es[][EDGE_PROPERTY], int cs[][CELL_PROPERTY], int eid){
-}
-
-
+// クリティカルパスを求める
+void trace_critical_path(int *g[GRAPH_ITEM_NUM], int vs[][VERTEX_PROPERTY], int es[][EDGE_PROPERTY], int cs[][CELL_PROPERTY], int task_num);
 #endif
---
+++ kadai4/kadai4/main.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include "array_graph.h"
+#include "kadai4.h"
+
+void main(void)
+{
+	int *g[GRAPH_ITEM_NUM];
+	int vs[VERTEX_NUM][VERTEX_PROPERTY];
+	int es[EDGE_NUM][EDGE_PROPERTY];
+	int cs[CELL_NUM][CELL_PROPERTY];
+	char *fname = "graph_kadai4.dat";
+	int vid, v1, v2, eid;
+
+	init_graph(g, vs, es, cs);
+	read_data(g, fname);
+	printf("Edge Data\n");
+	pr_edge_all(g);
+	printf("Vertex Data\n");
+	pr_vertex_all(g);
+
+	calc_earliest_node_times(g, vs, es, cs, 8);
+
+	calc_latest_node_times(g, vs, es, cs, 8);
+
+	trace_critical_path(g, vs, es, cs, 8);
+
+	system("pause");
+}
